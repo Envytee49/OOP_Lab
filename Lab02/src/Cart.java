9ad71ebd196da462b3d41e5358625aecb1b4fddf
Envytee49 <91842142+Envytee49@users.noreply.github.com>
2023-04-30 16:12:56 +0700

    Add files via upload

---
+++ Lab02/src/Cart.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class Cart {
+    public static final int MAXIMUM_DVDS = 20;
+    private int dvdQuantity = 0;
+    private ArrayList<DigitalVideoDisc> dvdList = new ArrayList<>();
+    public void addDVD(DigitalVideoDisc disc) {
+        if (dvdQuantity == MAXIMUM_DVDS) {
+            System.out.println("The cart is almost full");
+            return;
+        }
+        dvdList.add(disc);
+        dvdQuantity++;
+        System.out.println("The disc has been added");
+    }
+
+    public void removeDVD(DigitalVideoDisc disc) {
+        if(dvdList.contains(disc)) {
+        	dvdList.remove(disc);
+        }else {
+        	System.out.println("The disc has not been found");
+        }
+    }
+
+    public float totalCost() {
+        float total = 0;
+        for (int i = 0; i < dvdList.size(); i++) {
+            total += dvdList.get(i).getCost();
+        }
+        return total;
+    }
+}
